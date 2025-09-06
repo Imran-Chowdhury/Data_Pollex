0a0c30518edfb7b1d28f2d7836f8d195e1075d53
Imran Chowdhury <imranchowdhury6093@gmail.com>
2025-09-06 21:51:42 +0600

    feat: Stripe necessary setup

--- android/app/src/main/kotlin/com/example/data_pollex/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/data_pollex/MainActivity.kt
@@ -1,5 +1,12 @@
+//package com.example.data_pollex
+//
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity: FlutterActivity()
 package com.example.data_pollex
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
