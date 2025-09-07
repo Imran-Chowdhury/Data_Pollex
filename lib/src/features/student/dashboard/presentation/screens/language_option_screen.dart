457a0b7b29021fd559db3d68594cc7fc00fd6292
Imran Chowdhury <imranchowdhury6093@gmail.com>
2025-09-07 22:16:58 +0600

    Language option screen updated

--- lib/src/features/student/dashboard/presentation/screens/language_option_screen.dart
+++ lib/src/features/student/dashboard/presentation/screens/language_option_screen.dart
@@ -1,7 +1,8 @@
-import 'package:data_pollex/src/features/student/available_teachers/presentation/screens/available_teacher_screen.dart';
+import 'package:data_pollex/src/widgets/background_container.dart';
 import 'package:flutter/material.dart';
 
-import '../../../booked_lessons/presentation/screens/booked_lesson_screen.dart';
+import '../../../../../core/utils/color.dart';
+import '../widgets/lesson_list_student.dart';
 
 class LanguageOptionScreen extends StatelessWidget {
   const LanguageOptionScreen({super.key, required this.whereTo});
@@ -10,84 +11,30 @@ class LanguageOptionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final isLargeScreen = size.width > 600;
+    // final size = MediaQuery.of(context).size;
+    // final isLargeScreen = size.width > 600;
 
-    final languages = [
-      {'name': 'English', 'icon': Icons.language},
-      {'name': 'Bangla', 'icon': Icons.translate},
-      {'name': 'French', 'icon': Icons.school},
-      {'name': 'Russian', 'icon': Icons.public},
-    ];
+    // final languages = [
+    //   {'name': 'English', 'icon': Icons.language},
+    //   {'name': 'Bangla', 'icon': Icons.translate},
+    //   {'name': 'French', 'icon': Icons.school},
+    //   {'name': 'Russian', 'icon': Icons.public},
+    // ];
 
     return Scaffold(
+      backgroundColor: CustomColor.primary,
       appBar: AppBar(
-        title: const Text("Teacher Availability"),
-        backgroundColor: Colors.deepPurple,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: isLargeScreen ? 4 : 2,
-            crossAxisSpacing: 16,
-            mainAxisSpacing: 16,
-            childAspectRatio: 1,
+        backgroundColor: CustomColor.primary,
+        title: const Text(
+          "Select Lesson",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: CustomColor.white,
           ),
-          itemCount: languages.length,
-          itemBuilder: (context, index) {
-            final lang = languages[index];
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => whereTo == 'Teacher Availability'
-                        ? TeacherAvailabilityScreen(
-                            language: lang['name'] as String)
-                        : BookedSchedulesScreen(
-                            language: lang['name'] as String,
-                          ),
-                  ),
-                );
-              },
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16)),
-                elevation: 4,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.purpleAccent.withOpacity(0.7),
-                        Colors.purpleAccent
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(lang['icon'] as IconData,
-                          size: 48, color: Colors.white),
-                      const SizedBox(height: 16),
-                      Text(
-                        lang['name'] as String,
-                        style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
         ),
+      ),
+      body: BackgroundContainer(
+        child: StudentLessonList(whereTo: whereTo),
       ),
     );
   }
---
+++ lib/src/features/student/dashboard/presentation/widgets/lesson_list_student.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../../widgets/flag_tile.dart';
+import '../../../available_teachers/presentation/screens/available_teacher_screen.dart';
+import '../../../booked_lessons/presentation/screens/booked_lesson_screen.dart';
+
+class StudentLessonList extends ConsumerWidget {
+  const StudentLessonList({super.key, required this.whereTo});
+  final String whereTo;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final state = ref.watch(manageLessonsProvider);
+
+    final languages = [
+      'Bangla',
+      'French',
+      'Russian',
+      'English',
+    ];
+
+    return ListView.builder(
+      padding: const EdgeInsets.all(16), // optional padding inside container
+      itemCount: languages.length,
+      itemBuilder: (context, i) {
+        final langs = languages[i];
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => whereTo == 'Teacher Availability'
+                      ? TeacherAvailabilityScreen(language: langs)
+                      : BookedSchedulesScreen(language: langs),
+                ),
+              );
+            },
+            child: FlagTile(
+              countryName: langs,
+              flagAssetPath: 'assets/$langs.png',
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
